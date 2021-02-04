7b4c1ec7fb688f8c5e78b7805fc299372a3bebd7
patiljayant <72199264+patiljayant@users.noreply.github.com>
2021-02-04 17:29:45 +0530

    Add files via upload

---
+++ dbConfig/keys/keys.js
@@ -0,0 +1,6 @@
+const keys= {
+    uri : 'mongodb+srv://contactjay:Jayant@012@winter-project.2ydna.mongodb.net/Ecom?retryWrites=true&w=majority',
+    razorpayIdKey : 'rzp_test_Rr55cqI1se8NcK',
+    razorpaySecretKey : 'mNs1XtsfDJ8mBTrt3jRtUmty'
+}
+module.exports = keys;
